acbaf37ba6cd64c76cb32898603b32d680828451
Yuvraj Chhetri <yuvichh01@gmail.com>
2023-11-07 07:22:34 +0000

    added terraform and kubectl scripts

--- script.sh
+++ script.sh
@@ -8,4 +8,16 @@ sudo apt install docker-ce
 sudo systemctl status docker
 sudo usermod -aG docker ${USER} //adding user to the docker group to avoid sudo command
 
+# install terraform
+sudo apt install wget -y
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+# install Kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 
